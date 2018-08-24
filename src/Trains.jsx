bb548efcf21bf9e63f8b974adde061af81362765
Jim Gallagher <gallagherji@dnb.com>
2018-08-24 13:31:58 +0100

    Luas working correctly

--- src/Trains.jsx
+++ src/Trains.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-
-import {parse} from 'fast-xml-parser';
+//import {parseString} from 'xml2js';
+import {parse} from 'pixl-xml';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import axios from "axios";
@@ -12,12 +12,11 @@ export default class Trains extends React.Component {
 
     constructor(props) {
         super(props);
-
         this.state = {
-            glencairnData: {message: '', inboundTrains: [], outboundTrains: []},
-            gallopsData: {message: '', inboundTrains: [], outboundTrains: []},
-            leopardstownData: {message: '', inboundTrains: [], outboundTrains: []},
-            ballyoganData: {message: '', inboundTrains: [], outboundTrains: []}
+            glencairnData: {message: '', trainData: {inboundTrains: [], outboundTrains: []}},
+            gallopsData: {message: '', trainData: {inboundTrains: [], outboundTrains: []}},
+            leopardstownData: {message: '', trainData: {inboundTrains: [], outboundTrains: []}},
+            ballyoganData: {message: '', trainData: {inboundTrains: [], outboundTrains: []}}
         }
         axios.all([
             axios.get(API_ROOT + '?encrypt=false&action=forecast&stop=GLE'),
@@ -36,49 +35,43 @@ export default class Trains extends React.Component {
                     leopardstownData: leopardstownData,
                     ballyoganData: ballyoganData
                 });
-                console.log(this.state.gallopsData);
             }));
     }
 
     processTrainData(response) {
-        // result = parse(response.data);
-        parseString(response.data, function (err, result) {
-            console.log(result.stopInfo.direction);
-            let data = this.gatherResponseData(result);
-            return {
-                message: data.message,
-                trainData: {
-                    inboundTrains: data.inboundTrains,
-                    outboundTrains: data.outboundTrains
-                }
-            }
-        });
-    }
-
-    gatherResponseData(result) {
+        let result = parse(response.data);
         let inboundTrains = [];
         let outboundTrains = [];
-        result.stopInfo.direction.forEach(entry => {
+        result.direction.forEach(entry => {
             let target = [];
-            entry.tram.forEach(tram => {
+            if (Array.isArray(entry.tram)) {
+                entry.tram.forEach(tram => {
+                    let tramEntry = {
+                        dueMins: tram.dueMins,
+                        destination: tram.destination
+                    };
+                    target.push(tramEntry);
+                })
+            } else {
                 let tramEntry = {
-                    dueMins: tram['$']['dueMins'],
-                    destination: tram['$']['destination']
-                }
+                    dueMins: entry.tram.dueMins,
+                    destination: entry.tram.destination
+                };
                 target.push(tramEntry);
-            });
-            if (entry['$']['name'] === 'Inbound') {
+            }
+            if (entry.name === 'Inbound') {
                 inboundTrains = target;
             } else {
                 outboundTrains = target;
             }
-        })
+        });
         return {
-            selectedStation: result['stopInfo']['$']['stop'],
-            message: result['stopInfo']['message'],
-            inboundTrains: inboundTrains,
-            outboundTrains: outboundTrains,
-        };
+            message: result.message,
+            trainData: {
+                inboundTrains: inboundTrains,
+                outboundTrains: outboundTrains
+            }
+        }
     }
 
     generateTrainRows(trainData) {
@@ -99,24 +92,76 @@ export default class Trains extends React.Component {
         return (
             <div>
                 <div id="luas">
-                    <h3>Luas Information</h3>
-                    <div>General Line notice: {this.state.glencairnData.message}</div>
-                    <div>
-                        <table className="table table-bordered table-striped">
-                            <thead>
-                            <tr>
-                                <th colSpan="2">Glencairn</th>
-                            </tr>
-                            <tr>
-                                <th>Time</th>
-                                <th>Destination</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {this.generateTrainRows(this.state.glencairnData.inboundTrains)}
-                            {this.generateTrainRows(this.state.glencairnData.outboundTrains)}
-                            </tbody>
-                        </table>
+                    <h3 class="text-center">Luas Information - {this.state.glencairnData.message}</h3>
+                    <div class="row">
+                        <div class="col">
+                            <table className="table table-bordered table-striped">
+                                <thead>
+                                <tr>
+                                    <th colSpan="2">Glencairn</th>
+                                </tr>
+                                <tr>
+                                    <th>Time</th>
+                                    <th>Destination</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {this.generateTrainRows(this.state.glencairnData.trainData.inboundTrains)}
+                                {this.generateTrainRows(this.state.glencairnData.trainData.outboundTrains)}
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="col">
+                            <table className="table table-bordered table-striped">
+                                <thead>
+                                <tr>
+                                    <th colSpan="2">The Gallops</th>
+                                </tr>
+                                <tr>
+                                    <th>Time</th>
+                                    <th>Destination</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {this.generateTrainRows(this.state.gallopsData.trainData.inboundTrains)}
+                                {this.generateTrainRows(this.state.gallopsData.trainData.outboundTrains)}
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="col">
+                            <table className="table table-bordered table-striped">
+                                <thead>
+                                <tr>
+                                    <th colSpan="2">Leopardstown Valley</th>
+                                </tr>
+                                <tr>
+                                    <th>Time</th>
+                                    <th>Destination</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {this.generateTrainRows(this.state.leopardstownData.trainData.inboundTrains)}
+                                {this.generateTrainRows(this.state.leopardstownData.trainData.outboundTrains)}
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="col">
+                            <table className="table table-bordered table-striped">
+                                <thead>
+                                <tr>
+                                    <th colSpan="2">Ballyogan Wood</th>
+                                </tr>
+                                <tr>
+                                    <th>Time</th>
+                                    <th>Destination</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {this.generateTrainRows(this.state.ballyoganData.trainData.inboundTrains)}
+                                {this.generateTrainRows(this.state.ballyoganData.trainData.outboundTrains)}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
                 <div id="buses"></div>
